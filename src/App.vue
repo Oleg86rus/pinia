d689db2722d9f207b5c6b524c55db07222499298
oleg086rus <o.pototskiy@yandex.ru>
2023-01-16 15:11:50 +0500

    add adctions

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,12 @@
       <h2>My Favourite Movies</h2>
     </header>
     <div class="tabs">
-      <button :class="['btn', {btn_green: movieStore.activeTab === 1}]">Favourite</button>
-      <button :class="['btn', {btn_green: movieStore.activeTab === 2}]">Search</button>
+      <button :class="['btn', {btn_green: movieStore.activeTab === 1}]" @click="setTab(1)">Favourite</button>
+      <button :class="['btn', {btn_green: movieStore.activeTab === 2}]" @click="setTab(2)">Search</button>
     </div>
     <div class="movies" v-if="movieStore.activeTab === 1">
       <div>
-        <h3>Watched movies (count: {{ movieStore.watchedMovies }})</h3>
+        <h3>Watched movies (count: {{ movieStore.watchedMovies.length }})</h3>
         <Movie
             v-for="m of movieStore.watchedMovies"
             :key="m.id"
@@ -32,6 +32,7 @@
 import { useMovieStore } from "./stores/MovieStore";
 import Movie from './components/Movie.vue'
 
+const setTab = (id) => movieStore.setActiveTab(id)
 const movieStore = useMovieStore();
 </script>
 
